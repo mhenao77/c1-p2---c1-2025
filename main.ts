bf3bc4af6282ac38e2fe338400c8d60ad7f016f0
mhenao77 <128107897+mhenao77@users.noreply.github.com>
2025-11-15 12:52:28 -0500

    Practica con botones A, B, AB, Agitado

--- main.ts
+++ main.ts
@@ -1,3 +1,56 @@
+let CAMINAR = ""
+let cortar = ""
+input.onButtonPressed(Button.A, function () {
+    CAMINAR = "SI"
+})
+input.onGesture(Gesture.Shake, function () {
+    cortar = "SI"
+})
+input.onButtonPressed(Button.AB, function () {
+    cortar = "NO"
+})
+input.onButtonPressed(Button.B, function () {
+    CAMINAR = "NO"
+})
 basic.forever(function () {
-	
+    while (CAMINAR == "SI") {
+        basic.showLeds(`
+            . . . # #
+            . . . # .
+            # . . # .
+            . # # # .
+            . # . # .
+            `)
+        basic.pause(1000)
+        basic.showLeds(`
+            . . . # #
+            . . . # .
+            # . . # .
+            . # # # .
+            . . # . #
+            `)
+        basic.pause(1000)
+        basic.clearScreen()
+    }
+})
+basic.forever(function () {
+    while (cortar == "SI") {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+        basic.pause(1000)
+        basic.showLeds(`
+            # # . . .
+            # # . . .
+            . . # # #
+            # # . . .
+            # # . . .
+            `)
+        basic.pause(1000)
+        basic.clearScreen()
+    }
 })
